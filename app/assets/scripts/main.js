5161d0047e5cf33288bfb7ec1ae1386dfd64f3fc
Drew Bollinger <drew@developmentseed.org>
2016-05-24 12:27:48 -0400

    Add 'functioning' app

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict'
-var config = require('./config')
 
-console.log.apply(console, config.consoleMessage)
-console.log('Environment', config.environment)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import config from './config'
+
+// Views
+import App from './views/app'
+
+const logger = createLogger({
+  level: 'info',
+  collapsed: true,
+  predicate: (getState, action) => {
+    return (config.environment !== 'production')
+  }
+})
+const store = createStore(
+  combineReducers({
+    reducer: (state = {}, action) => state,
+    routing: routerReducer
+  }, applyMiddleware(thunk, logger))
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={App}>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('site-canvas')
+)
---
+++ app/assets/scripts/views/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = React.createClass({
+
+  render: function () {
+    return (
+      <div className='full-app'>
+        The App is Functioning
+      </div>
+    )
+  },
+
+  propTypes: {}
+})
+
+export default App
