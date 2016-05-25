3cfbc25147387dea0ed406f6307c94c0598817b8
Drew Bollinger <drew@developmentseed.org>
2016-05-25 15:00:18 -0400

    Fix route reducer name

--- app/assets/scripts/reducers/index.js
+++ app/assets/scripts/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux'
-import { routeReducer } from 'react-router-redux'
+import { routerReducer } from 'react-router-redux'
 
 import reducer from './reducer'
 
 export default combineReducers({
   reducer,
-  routing: routeReducer
+  routing: routerReducer
 })
