6f43d06f8850d7475c23432349617a2f69114b88
Drew Bollinger <drew@developmentseed.org>
2016-05-25 15:07:49 -0400

    Add action templates

--- app/assets/scripts/reducers/reducer.js
+++ app/assets/scripts/reducers/reducer.js
@@ -1,3 +1,5 @@
+import { UPDATE_FOO, UPDATE_BAR, REDUCER_TO_DEFAULT } from '../actions'
+
 export const initial = {
   foo: 1,
   bar: 2
@@ -5,11 +7,11 @@ export const initial = {
 
 export default function reducer (state = initial, action) {
   switch (action.type) {
-    case 'UPDATE_FOO':
+    case UPDATE_FOO:
       return Object.assign({}, state, { foo: action.data })
-    case 'UPDATE_BAR':
+    case UPDATE_BAR:
       return Object.assign({}, state, { bar: action.data })
-    case 'REDUCER_TO_DEFAULT':
+    case REDUCER_TO_DEFAULT:
       return initial
     default:
       return state
