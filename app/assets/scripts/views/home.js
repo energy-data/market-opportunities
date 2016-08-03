0f42064ac79f965181dde32a9e9cf976c35dbe1e
Ricardo Mestre <ricardo@duplos.org>
2016-08-03 14:37:35 +0100

    Homepage design tweaks (#83)
    * Update icon library
    * Update home fold and button colors
    * Update screenshot
    * Fix feature list spacing
    * Revamp fold and media styles
    * Improve layout title size and spacing
    * Reorder showcase fold elements
    * Replace screenshot
    * Replace hero illustration

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -21,13 +21,13 @@ const Home = React.createClass({
                     <h1 className='layout__title'>Discover Off-Grid Energy Opportunities</h1>
                     <div className='layout__introduction'>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in enim ut nunc iaculis gravida quis in ante. Ut eu turpis eget leo placerat consequat lorem ipsum.</p>
-                      <p><Link to='/explore' title='Visit Explore page' className='button button--xlarge button--base'><span>Start exploring</span></Link></p>
+                      <p><Link to='/explore' title='Visit Explore page' className='cta-button-explore'><span>Start exploring</span></Link></p>
                     </div>
                   </div>
                 </div>
               </header>
               <div className='layout__body'>
-                <section className='fold fold--base fold--copy-right' id='showcase-fold'>
+                <section className='fold fold--copy-left' id='showcase-fold'>
                   <div className='inner'>
                     <div className='fold__copy'>
                       <header className='fold__header'>
@@ -42,16 +42,16 @@ const Home = React.createClass({
                         </ul>
                       </div>
                     </div>
-                    <div className='fold__media fold__media--desktop'>
-                      <figure className='media-item'>
-                        <div className='media-frame'>
+                    <div className='fold__media'>
+                      <figure className='media-content'>
+                        <div className='media-item media-item--browser'>
                           <img src='/assets/graphics/content/tool-screenshot-ogmo.jpg' alt='Fold image' width='1920' height='960' />
                         </div>
                       </figure>
                     </div>
                   </div>
                 </section>
-                <section className='fold fold--copy-center' id='locations-fold'>
+                <section className='fold fold--primary fold--copy-center' id='locations-fold'>
                   <div className='inner'>
                     <div className='fold__copy'>
                       <header className='fold__header'>
@@ -64,7 +64,7 @@ const Home = React.createClass({
                           <li><strong className='value'>56</strong> <span className='term'>Countries</span></li>
                           <li><strong className='value'>327</strong> <span className='term'>Dolor sit</span></li>
                         </ul>
-                        <p><Link to='/about' title='Visit About page' className='button button--large button--primary'><span>Learn more</span></Link></p>
+                        <p><Link to='/about' title='Visit About page' className='cta-button-learn'><span>Learn more</span></Link></p>
                       </div>
                     </div>
                   </div>
